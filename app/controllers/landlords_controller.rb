1cc6bd7b308e04f2782978a101748fc32b142fe2
Alfredo Pantoja <chololoy@gmail.com>
2013-10-30 14:51:45 -0500

    Finish landlord ceation

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -1,4 +1,26 @@
 class LandlordsController < ApplicationController
-  def new
+
+  def show
+    @landlord = Landlord.find(params[:id])
   end
+
+  def new
+    @landlord = Landlord.new
+  end
+
+  def create
+    @landlord = Landlord.new(landlord_params)
+    if @landlord.save
+      flash[:success] = "New landlord created."
+      redirect_to @landlord
+    else
+      render 'new'
+    end
+  end  
+
+  private
+
+    def landlord_params
+      params.require(:landlord).permit(:name, :email, :phone)
+    end  
 end
--- spec/requests/landlord_pages_spec.rb
+++ spec/requests/landlord_pages_spec.rb
@@ -4,10 +4,42 @@ describe "Landlord pages" do
 
   subject { page }
 
+  describe "show page" do
+    let(:landlord) { FactoryGirl.create(:landlord) }
+    before { visit landlord_path(landlord) }
+
+    it { should have_content(landlord.name) }
+    it { should have_title(landlord.name) }
+  end  
+
   describe "new page" do
     before { visit new_landlord_path }
 
     it { should have_content('Create new landlord') }
     it { should have_title(full_title('Create new landlord')) }
+  end  
+
+  describe "new landlord signup" do
+    before { visit new_landlord_path }
+
+    let(:submit) { "Create new landlord" }
+
+    describe "with invalid information" do
+      it "should not create a landlord" do
+        expect { click_button submit }.not_to change(Landlord, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",  with: "Example Landlord"
+        fill_in "Email", with: "landlord@example.com"
+        fill_in "Phone", with: "312-555-4857"
+      end  
+
+      it "should create a landlord" do
+        expect { click_button submit }.to change(Landlord, :count).by(1)
+      end
+    end
   end  
 end
